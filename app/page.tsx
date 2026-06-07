e540c6afa6ee1343cf3df0d6fcb9f4869a024ff1
sstt126 <g2453828@cc.kyoto-su.ac.jp>
2026-06-07 15:31:54 +0900

    登録内容をサイトから変更可能に

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,9 @@ import { useState, useRef } from 'react';
 
 export default function Home() {
   const [preview, setPreview] = useState<string | null>(null);
-  const [data, setData] = useState<any>(null);
+  const [data, setData] = useState<CalendarData | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const fileInputRef = useRef<HTMLInputElement>(null);
-  
 
   type CalendarData = {
     text: string;
@@ -23,6 +22,22 @@ export default function Home() {
     minutes_end: string;
     location: string;
   };
+
+  const [pieceData, setPieceData] = useState<CalendarData>({
+    text: "",
+    details: "",
+    year_start: "",
+    month_start: "",
+    date_start: "",
+    year_end: "",
+    month_end: "",
+    date_end: "",
+    hours_start: "",
+    minutes_start: "",
+    hours_end: "",
+    minutes_end: "",
+    location: "",
+  });
 
   // ① 画像が選択されたら、ブラウザ上にプレビューを表示する処理
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -61,6 +76,21 @@ export default function Home() {
 
       const result = await response.json();
       setData(result);
+      setPieceData({
+        text: result.text,
+        details: result.details,
+        year_start: result.year_start,
+        month_start: result.month_start,
+        date_start: result.date_start,
+        year_end: result.year_end,
+        month_end: result.month_end,
+        date_end: result.date_end,
+        hours_start: result.hours_start,
+        minutes_start: result.minutes_start,
+        hours_end: result.hours_end,
+        minutes_end: result.minutes_end,
+        location: result.location,
+      });
     } catch (error) {
       console.error("エラー:", error);
     } finally {
@@ -69,22 +99,8 @@ export default function Home() {
 
     
   };
-  const formatDate = (data: CalendarData) => {
-    if (
-      data.year_start === data.year_end &&
-      data.month_start === data.month_end &&
-      data.date_start === data.date_end
-    ){
-      return `${data.year_start}/${data.month_start}/${data.date_start}`;
-    }else {
-      return `${data.year_start}/${data.month_start}/${data.date_start} ～ ${data.year_end}/${data.month_end}/${data.date_end}`;
-    };
-  };
-  const formatTime = (data: CalendarData) => {
-    return `${data.hours_start}:${data.minutes_start} ～ ${data.hours_end}:${data.minutes_end}`;
-  };
 
-  const makeUrl = (data: CalendarData) => {
+  const makeUrl = () => {
     const {
       text,
       details,
@@ -99,7 +115,7 @@ export default function Home() {
       hours_end,
       minutes_end,
       location,
-    } = data;
+    } = pieceData;
 
     const start = `${year_start}${month_start}${date_start}T${hours_start}${minutes_start}00`;
     const end = `${year_end}${month_end}${date_end}T${hours_end}${minutes_end}00`;
@@ -116,7 +132,7 @@ export default function Home() {
     return `https://calendar.google.com/calendar/render?${params}`;
   };
   const handleOpen = () => {
-    const url = makeUrl(data);
+    const url = makeUrl();
 
     window.open(url, "_blank");
   };
@@ -154,11 +170,140 @@ export default function Home() {
       {/* バックエンドからの結果表示 */}
       {data && (
         <div style={{ marginTop: "20px", color: "white" }}>
-          <h3>件名: {data.text}</h3>
-          <h3>詳細: {data.details}</h3>
-          <h3>日付: {formatDate(data)}</h3>
-          <h3>時間: {formatTime(data)}</h3>
-          <h3>場所: {data.location}</h3>
+          
+          <span>件名：</span>
+            <input
+              value={pieceData.text}
+              onChange={(e) => setPieceData({...pieceData,text: e.target.value})}
+              placeholder='件名'
+              style={{
+                width: '90%',
+              }}
+            />
+          <div style={{ display: 'flex', alignItems: 'flex-start' }}>
+            <span>詳細：</span>
+              <textarea
+                value={pieceData.details}
+                onChange={(e) => setPieceData({ ...pieceData, details: e.target.value })}
+                placeholder='詳細'
+                style={{
+                  width: '90%',
+                }}
+              />
+              </div>
+          <div>
+              <span>開始日：</span>
+              <input
+                value={pieceData.year_start}
+                onChange={(e) => setPieceData({ ...pieceData, year_start: e.target.value })}
+                placeholder='開始年'
+                style={{
+                  width: '40px',
+                  textAlign: 'center',
+                }}
+              />
+              <span>/</span>
+              <input
+                value={pieceData.month_start}
+                onChange={(e) => setPieceData({ ...pieceData, month_start: e.target.value })}
+                placeholder='開始月'
+                style={{
+                  width: '20px',
+                  textAlign: 'center',
+                }}
+              />
+              <span>/</span>
+              <input
+                value={pieceData.date_start}
+                onChange={(e) => setPieceData({ ...pieceData, date_start: e.target.value })}
+                placeholder='開始日'
+                style={{
+                  width: '20px',
+                  textAlign: 'center',
+                }}
+              />
+                <span>　　　終了日：</span>
+                <input
+                  value={pieceData.year_end}
+                  onChange={(e) => setPieceData({ ...pieceData, year_end: e.target.value })}
+                  placeholder='終了年'
+                  style={{
+                    width: '45px',
+                    textAlign: 'center',
+                  }}
+                />
+                <span>/</span>
+                <input
+                  value={pieceData.month_end}
+                  onChange={(e) => setPieceData({ ...pieceData, month_end: e.target.value })}
+                  placeholder='終了月'
+                  style={{
+                    width: '20px',
+                    textAlign: 'center',
+                  }}
+                />
+                <span>/</span>
+                <input
+                  value={pieceData.date_end}
+                  onChange={(e) => setPieceData({ ...pieceData, date_end: e.target.value })}
+                  placeholder='終了日'
+                  style={{
+                    width: '20px',
+                    textAlign: 'center',
+                  }}
+                />
+          </div>
+          <div>
+            <span>開始時間：</span>
+            <input
+              value={pieceData.hours_start}
+              onChange={(e) => setPieceData({ ...pieceData, hours_start: e.target.value })}
+              placeholder='開始時'
+              style={{
+                width: '20px',
+                textAlign: 'center',
+              }}
+            />
+            <span>:</span>
+            <input
+              value={pieceData.minutes_start}
+              onChange={(e) => setPieceData({ ...pieceData, minutes_start: e.target.value })}
+              placeholder='開始分'
+              style={{
+                width: '20px',
+                textAlign: 'center',
+              }}
+            />
+            <span>　　　終了時間：</span>
+            <input
+              value={pieceData.hours_end}
+              onChange={(e) => setPieceData({ ...pieceData, hours_end: e.target.value })}
+              placeholder='終了時'
+              style={{
+                width: '20px',
+                textAlign: 'center',
+              }}
+            />
+            <span>:</span>
+            <input
+              value={pieceData.minutes_end}
+              onChange={(e) => setPieceData({ ...pieceData, minutes_end: e.target.value })}
+              placeholder='終了分'
+              style={{
+                width: '20px',
+                textAlign: 'center',
+              }}
+            />
+          </div>
+          <span>場所：</span>
+          <input
+            value={pieceData.location}
+            onChange={(e) => setPieceData({ ...pieceData, location: e.target.value })}
+            placeholder='場所'
+            style={{
+              width: '90%',
+            }}
+          />
         </div>
       )}
 
